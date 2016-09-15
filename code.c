9397c1aefbec21716e6f052c3bbfc0152605757d
traceyma <traceyma@fortinet.com>
2016-09-15 15:48:41 -0700

    [CC] add .gitignore, update two functions in 1.1.

--- code.c
+++ code.c
@@ -4,14 +4,20 @@
 #include <string.h>
 
 int isUniqueChars(const char *str);
+int isUniqueChars2(const char *str);
+int isUniqueChars3(const char *arr);
 
 int main(int argc, const char* argv[]) {
 	int i, test;
-	char *str;
 
-	for (i = 0; i < argc; i++)
-		printf("argv[%d] = %s\n", i, argv[i]);
-	test = isUniqueChars(argv[1]);
+	for (i = 0; i < argc; i++) {
+		int len = strlen(argv[i]);
+		printf("argv[%d] = %s, len = %d\n", i, argv[i], len);
+	}
+	
+	//test = isUniqueChars(argv[1]); /*1.1-1*/
+	//test = isUniqueChars2(argv[1]); /*1.1-2*/
+	//test = isUniqueChars3(argv[1]); /*1.1-3*/
 	printf("Result: %d\n", test);
 	return 0;
 }
@@ -28,3 +34,28 @@ int isUniqueChars(const char *str) {
 	}
 	return 1;
 }
+
+
+int isUniqueChars2(const char *str) {
+	int i, j;
+	int len = strlen(str);
+	for (i = 0; i < len - 1; i++) {
+		for (j = i+1 ; j < len; j++) {
+			if (str[i] == str[j])
+				return false;
+		}
+	}
+	return true;
+}
+
+int isUniqueChars3(const char *arr){
+	int i, val;
+	for (i = 0; i < strlen(arr); i++){
+		int index = arr[i] - 'a';
+		if ((val & (1 << index)) > 0)
+			return false; 
+		val |= 1 << index;
+	}
+	return true;
+}
+
