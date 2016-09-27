d4936db7644ed09915bf06c6ec9b05191dec7f5a
traceyma <traceyma@fortinet.com>
2016-09-26 17:30:02 -0700

    [CC] updated array.

--- code.c
+++ code.c
@@ -6,6 +6,8 @@
 int isUniqueChars(const char *str);
 int isUniqueChars2(const char *str);
 int isUniqueChars3(const char *arr);
+void reverse(const char *str);
+void removeDupilicate(const char *str);
 
 int main(int argc, const char* argv[]) {
 	int i, test;
@@ -18,6 +20,8 @@ int main(int argc, const char* argv[]) {
 	//test = isUniqueChars(argv[1]); /*1.1-1*/
 	//test = isUniqueChars2(argv[1]); /*1.1-2*/
 	//test = isUniqueChars3(argv[1]); /*1.1-3*/
+	//reverse(argv[1]);
+	removeDupilicate(argv[1]);
 	printf("Result: %d\n", test);
 	return 0;
 }
@@ -59,3 +63,56 @@ int isUniqueChars3(const char *arr){
 	return true;
 }
 
+void reverse(const char *str) {
+	int len = strlen(str);
+	char *arr = malloc(sizeof(char) * len);
+	char *arr_mark = arr;
+
+	strcpy(arr, str);
+	printf("string: %s\n", arr);
+	char *end = arr;
+	char temp;
+	int i = 0;
+	if (arr) {
+		while (*end)
+			end++;
+		end--;
+		
+		while (arr < end) {
+			temp = *arr;
+			*arr = *end; 
+			*end = temp;
+			arr++;
+			end--;
+		}
+	}
+	printf("reverse string: %s\n", arr_mark);
+	free(arr_mark);
+}
+
+void removeDupilicate(const char *str) {
+	if (str == NULL) return;
+	int len = strlen(str);
+	if (len < 2) return;
+
+	char *arr = (char *)malloc(sizeof(char) * len);
+	if (arr == NULL)
+		return;
+	strcpy(arr, str);
+	printf("string: %s\n", arr);
+
+	int tail = 1, i;
+	for (i = 1; i < len; i++) {
+		int j;
+		for (j = 0; j < tail; j++) {
+			if (arr[i] == arr[j]) break; // j < tail
+		}
+		if (j == tail) {
+			arr[tail] = arr[i];
+			tail++;
+		}
+	printf("i: %d, j: %d, tail: %d\n", i, j, tail);
+	}
+	arr[tail] = 0;
+	printf("new array is: %s\n", arr);
+}
